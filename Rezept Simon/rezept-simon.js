b5a7622219506d9feb3a144c2dd7ee49c2efa313
Simon Wilmink <wilminksimon@googlemail.com>
2026-03-06 00:56:40 +0100

    add new "rezept Simon", in der index.html eingebunden

---
+++ Rezept Simon/rezept-simon.js
@@ -0,0 +1,38 @@
+    let möhren = 150;
+    let kartoffeln = 150;
+    let zwiebeln = 0.25;
+    let butter = 0.25;
+    let brühe = 150;
+    let wurst = 1;
+    let öl = 0.25;
+    let petersilie = 0.75;
+    let sahne = 0.5;
+
+
+    
+    
+
+function calculate() {
+    let neuePortionen = document.getElementById("portions").value;
+    
+    let möhrenNeu = möhren * neuePortionen;
+    let kartoffelnNeu = kartoffeln * neuePortionen;
+    let zwiebelnNeu = zwiebeln * neuePortionen;
+    let butterNeu = butter * neuePortionen;
+    let brüheNeu = brühe * neuePortionen;
+    let wurstNeu = wurst * neuePortionen;
+    let ölNeu = öl * neuePortionen;
+    let petersilieNeu = petersilie * neuePortionen;
+    let sahneNeu = sahne * neuePortionen;
+
+    document.getElementById("value1").innerHTML = möhrenNeu;
+    document.getElementById("value2").innerHTML = kartoffelnNeu;
+    document.getElementById("value3").innerHTML = zwiebelnNeu;
+    document.getElementById("value4").innerHTML = butterNeu;
+    document.getElementById("value5").innerHTML = brüheNeu;
+    document.getElementById("value6").innerHTML = wurstNeu;
+    document.getElementById("value7").innerHTML = ölNeu;
+    document.getElementById("value8").innerHTML = petersilieNeu;
+    document.getElementById("value9").innerHTML = sahneNeu;
+
+}
