d76df3eda3d5ba2f42a168460402f41f64678e40
Draco1882 <n.andreas.1982@gmx.de>
2026-03-08 13:58:42 +0100

    Änderungen an meinem Rezept

--- Rezept Andreas/rezept-andreas.js
+++ Rezept Andreas/rezept-andreas.js
@@ -1,14 +1,16 @@
 let menge = [500, 2, 5]
 let zutat = ['spagetti', 'tomaten', 'knoblauch']
-
-
+let today = new Date();
+let formattedDate = today.toLocaleDateString('de-DE');
+document.getElementById('date').innerHTML = formattedDate;
 
 function anzahl() {
     for (let a = 0; a < menge.length; a++) {
-        let personen = (document.getElementById("anzahl-personen").value);
+        let personen = (document.getElementById("number-of-people").value);
         let basis = menge[a];
         let element = document.getElementById(zutat[a]);
         element.textContent = basis * personen;
     }
 }
 anzahl();
+
