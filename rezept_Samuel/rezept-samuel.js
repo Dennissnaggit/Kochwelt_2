4a928e4263e5914eb85930ec7f080f6b091d86d2
Samuel <samuelschoenmeier@gmail.com>
2026-03-14 10:29:06 +0100

    mein JS hinzugefügt

--- rezept_Samuel/rezept-samuel.js
+++ rezept_Samuel/rezept-samuel.js
@@ -1,4 +1,36 @@
-const hamburgerMenu = document.getElementById("hamburgerMenu");
+let mehl = 125;
+let trockenhefe = 4;
+let zucker = 25;
+let milch = 60;
+let butter = 25;
+let braunerZucker = 30;
+let zimt = 2;
+let puderzucker = 50;
+
+
+function portions() {
+    let newValue = document.getElementById("input").value;
+
+    let mehlcalc = mehl * newValue;
+    let trockenhefecalc = trockenhefe * newValue;
+    let zuckercalc = zucker * newValue;
+    let milchcalc = milch * newValue;
+    let butterclac = butter * newValue;
+    let braunerZuckercalc = braunerZucker * newValue;
+    let zimtcalc = zimt * newValue;
+    let puderzuckercalc = puderzucker * newValue;
+
+    document.getElementById("value-1").innerHTML = mehlcalc;
+    document.getElementById("value-2").innerHTML = trockenhefecalc;
+    document.getElementById("value-3").innerHTML = zuckercalc;
+    document.getElementById("value-4").innerHTML = milchcalc;
+    document.getElementById("value-5").innerHTML = butterclac;
+    document.getElementById("value-6").innerHTML = braunerZuckercalc;
+    document.getElementById("value-7").innerHTML = zimtcalc;
+    document.getElementById("value-8").innerHTML = puderzuckercalc;
+}
+
+
 
 function openClose() {
     hamburgerMenu.classList.toggle("noneClass");
