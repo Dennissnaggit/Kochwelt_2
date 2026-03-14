ff7f0c21896c1dbba5a95cca11a6c31a4e00a4f8
Draco1882 <n.andreas.1982@gmx.de>
2026-03-14 10:06:23 +0100

    Resprosiev gemacht

--- Rezept Andreas/rezept-andreas.js
+++ Rezept Andreas/rezept-andreas.js
@@ -4,6 +4,8 @@ let zutat = ['mehl', 'milch', 'eier']
 let formattedDate = today.toLocaleDateString('de-DE');
 document.getElementById('date').innerHTML = formattedDate;*/
 
+let hamburgerMenu = document.getElementById("hamburgerMenu");
+
 function anzahl() {
     for (let a = 0; a < menge.length; a++) {
         let personen = (document.getElementById("number-of-people").value);
@@ -14,3 +16,6 @@ function anzahl() {
 }
 anzahl();
 
+function openClose() {
+    hamburgerMenu.classList.toggle("noneClass");
+}
