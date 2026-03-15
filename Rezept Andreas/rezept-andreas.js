75b1d1ccfcb6c07998b9d52517638da8efbd0754
Draco1882 <n.andreas.1982@gmx.de>
2026-03-15 15:01:53 +0100

    Die Änderungen die Gestern besprochen wurden umgesetzt

--- Rezept Andreas/rezept-andreas.js
+++ Rezept Andreas/rezept-andreas.js
@@ -6,12 +6,28 @@ document.getElementById('date').innerHTML = formattedDate;*/
 
 let hamburgerMenu = document.getElementById("hamburgerMenu");
 
-function anzahl() {
+
+function startAnzahl() {
     for (let a = 0; a < menge.length; a++) {
         let personen = (document.getElementById("number-of-people").value);
         let basis = menge[a];
         let element = document.getElementById(zutat[a]);
         element.textContent = basis * personen;
+    }
+}
+function anzahl() {
+    let personen = Number(document.getElementById("number-of-people").value);
+
+    if (personen >= 1 && personen <= 10){
+        
+        for (let a = 0; a < menge.length; a++) {
+            let basis = menge[a];
+            let element = document.getElementById(zutat[a]);
+            element.textContent = basis * personen;
+        }
+    }
+    else{
+        document.getElementById("wrongValue").innerHTML = "Bitte einen Wert zwischen 1 und 10 angeben";
     }
 }
 anzahl();
