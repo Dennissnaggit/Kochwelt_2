fd7c0ad1bd785b6a53449cc7607859579f4d529b
Draco1882 <n.andreas.1982@gmx.de>
2026-03-17 22:37:57 +0100

    Änderungen vom Feedback

--- Rezept Andreas/rezept-andreas.js
+++ Rezept Andreas/rezept-andreas.js
@@ -3,21 +3,16 @@ let zutat = ['mehl', 'milch', 'eier']
 
 
 let hamburgerMenu = document.getElementById("hamburgerMenu");
-
-
-function startAnzahl() {
-    for (let a = 0; a < menge.length; a++) {
-        let personen = (document.getElementById("number-of-people").value);
-        let basis = menge[a];
-        let element = document.getElementById(zutat[a]);
-        element.textContent = basis * personen;
-    }
+function init() {
+    anzahl();
 }
+
+
 function anzahl() {
     let personen = Number(document.getElementById("number-of-people").value);
 
     if (personen >= 1 && personen <= 10){
-        
+        document.getElementById("wrongValue").innerHTML = "";
         for (let a = 0; a < menge.length; a++) {
             let basis = menge[a];
             let element = document.getElementById(zutat[a]);
@@ -28,7 +23,7 @@ function anzahl() {
         document.getElementById("wrongValue").innerHTML = "Bitte einen Wert zwischen 1 und 10 angeben";
     }
 }
-anzahl();
+
 
 function openClose() {
     hamburgerMenu.classList.toggle("noneClass");
